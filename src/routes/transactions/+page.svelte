90049251d1aa2faa948141e5cfd2b0a9d0dd2e8d
Yikuan Sun <yikuansun2015@gmail.com>
2025-03-09 17:24:07 -0400

    Allow user to edit transactions
    Need to fix transaction sorting

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import UserData from "$lib/UserData";
     import { fade, slide } from "svelte/transition";
+    import moment from "moment";
 
     let data = new UserData();
 
@@ -30,13 +31,11 @@
      * @param {string} nickname title of the transaction (optional)
      */
     function createTransaction(dateString, amount, type, nickname="") {
-        // the HTML date input returns format yyyy-mm-dd - must be changed to mm/dd/yyyy
-        let dateYMD = (dateString || "2025-01-23").split("-"); // 01/23/2025 is the default value
         // expense is treated the same as negative income
         let quantityReal = amount || 0;
         if (type == "expense") quantityReal *= -1;
         data.transactions = [...data.transactions, {
-            date: dateYMD[1] + "/" + dateYMD[2] + "/" + dateYMD[0],
+            date: moment(dateString, "YYYY-MM-DD").format("MM/DD/YYYY"),
             quantity: quantityReal,
             nickname: nickname,
         }];
@@ -58,6 +57,41 @@
         if (date1[0] != date2[0]) return (date1[0] > date2[0])?-1:1; // if months are different, sort by month, otherwise
         if (date1[1] != date2[1]) return (date1[1] > date2[1])?-1:1; // if days are different, sort by day :)
         return 1;
+    }
+
+    let editModalOpen = false;
+    /** @type {{ dateString: string | null,
+     *          transactionType: "income" | "expense",
+     *          amount: number | null,
+     *          "nickName": string
+     *          transactionIndex: number }} */
+    let editModalData = {
+        transactionIndex: 0,
+        dateString: null,
+        transactionType: "income",
+        amount: null,
+        nickName: ""
+    };
+
+    /**
+     * Edit a transaction and save to UserData. Called when modal submit button pressed.
+     * @param {number} transactionIndex the index of the transaction to edit
+     * @param {string | null} dateString the date of the transaction, in HTML Date Input format (yyyy-mm-dd)
+     * @param {number | null} amount the magnitude of the transaction (absolute value)
+     * @param {"income" | "expense"} type decide if the amount be added to or substracted from balance
+     * @param {string} nickname title of the transaction (optional)
+     */
+    function editTransaction(transactionIndex, dateString, amount, type, nickname="") {
+        // expense is treated the same as negative income
+        let quantityReal = amount || 0;
+        if (type == "expense") quantityReal *= -1;
+        data.transactions[transactionIndex] = {
+            date: moment(dateString, "YYYY-MM-DD").format("MM/DD/YYYY"),
+            quantity: quantityReal,
+            nickname: nickname,
+        };
+        data.balance += quantityReal; // update balance
+        data.saveToLocalStorage(); // save after modification
     }
 </script>
 
@@ -91,12 +125,42 @@
     </div>
 {/if}
 
+{#if editModalOpen}
+    <button class="greywall" transition:fade={{ duration: 250, }}
+        on:click={() => { editModalOpen = false }}></button>
+    <div class="modal" transition:slide={{ duration: 500, }}>
+        <h2 style:margin-top="4px" style:margin-bottom="14px">Edit transaction</h2>
+        Date: <input type="date" bind:value={editModalData["dateString"]} /> <br />
+        Type:
+        <select bind:value={editModalData["transactionType"]}>
+            <option>income</option>
+            <option>expense</option>
+        </select> <br />
+        Amount: <input type="number" min={0} max={10000} bind:value={editModalData["amount"]} /> <br />
+        Title (optional): <input type="text" bind:value={editModalData["nickName"]} /> <br />
+        <button on:click={() => {
+            // add the transaction to user data
+            editTransaction(editModalData["transactionIndex"], editModalData["dateString"], editModalData["amount"], editModalData["transactionType"], editModalData["nickName"]);
+            // close modal
+            editModalOpen = false;
+        }}>Edit Transaction</button>
+    </div>
+{/if}
+
 <h2>Past transactions</h2>
-{#each data.transactions.toSorted(sortingCompareFn) as transaction}
+{#each data.transactions as transaction, transactionIndex}
     <p>
         {#if transaction["nickname"]} <b style:font-size="large">{transaction["nickname"]}</b> <br /> {/if}
         Date: {transaction["date"]} <br />
         Transaction type: {(transaction.quantity > 0)?"income":"expense"} <br />
-        Amount: {Math.abs(transaction.quantity)}
+        Amount: {Math.abs(transaction.quantity)} <br />
+        <button on:click={() => {
+            editModalData["transactionIndex"] = transactionIndex;
+            editModalData["dateString"] = moment(transaction["date"], "MM/DD/YYYY").format("YYYY-MM-DD");
+            editModalData["transactionType"] = (transaction.quantity > 0)?"income":"expense";
+            editModalData["amount"] = Math.abs(transaction.quantity);
+            editModalData["nickName"] = transaction["nickname"];
+            editModalOpen = true;
+        }}>Edit</button>
     </p>
 {/each}
