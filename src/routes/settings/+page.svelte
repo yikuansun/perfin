5aa682155674dcdd2017e00c480c4c4e0475f915
Yikuan Sun <yikuansun2015@gmail.com>
2025-03-12 09:01:27 -0400

    Update settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -10,10 +10,18 @@
 </script>
 
 <h2>Data and Privacy</h2>
+Data are only stored locally on your device. No one else can see your data.
+<br /> <br />
 <button on:click={() => {
     // clear data (dangerous action, so we ask the user first)
-    if (confirm("are u sure???")) {
+    if (confirm("This action is irreversible. Are you sure you want to clear your data?")) {
         data.purgeLocalStorage();
     }
 }}>Clear data</button>
 <hr />
+
+<style>
+    hr {
+        border: 1px solid #444444;
+    }
+</style>
